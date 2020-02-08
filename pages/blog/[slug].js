9c9d6f24cd541198e66412d87436cf245978ef4e
Ivan Kleshnin <ivan@paqmind.com>
2020-02-08 09:06:07 +0200

    Approximate data fetching

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,10 +2,8 @@ import {useRouter} from "next/router"
 import Head from "next/head"
 import React from "react"
 
-export default function Page(ctx) {
+export default function Page({title}) {
   let {query} = useRouter()
-
-  console.log("query:", query)
 
   let [loading, setLoading] = React.useState(true)
   let [data, setData] = React.useState(null)
@@ -24,8 +22,9 @@ export default function Page(ctx) {
   if (loading) {
     return <div>
       <Head>
-        <title>Post {query.slug}</title>
+        <title>{title}</title>
       </Head>
+      <h1>Post {query.slug}</h1>
       <p>
         Loading...
       </p>
@@ -37,12 +36,15 @@ export default function Page(ctx) {
   </div>
 }
 
-// Page.getInitialProps = function () {
-//   return {}
-// }
-
-export async function unstable_getStaticProps() {
+Page.getInitialProps = async function () {
+  let title = await Promise.resolve("Async Title")
   return {
-    props: {}
+    title
   }
 }
+
+// export async function unstable_getStaticProps() {
+//   return {
+//     props: {}
+//   }
+// }
